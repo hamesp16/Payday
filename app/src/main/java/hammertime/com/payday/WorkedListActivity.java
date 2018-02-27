2f03a2021bfdb178d5ee3f58b222cf66a644e261
hamesp16 <hamesp16@student.westerdals.no>
2018-02-27 13:12:49 +0100

    First commit

---
+++ app/src/main/java/hammertime/com/payday/WorkedListActivity.java
@@ -0,0 +1,15 @@
+package hammertime.com.payday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkedListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worked_list);
+    }
+}
